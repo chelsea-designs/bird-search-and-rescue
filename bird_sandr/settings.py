bc8b35e373d94f65d054c9335309c51b5b7a188e
chelsea-designs <hello@chelsea-designs.com>
2022-10-13 14:28:36 +0000

    remove secret key and set debug

--- bird_sandr/settings.py
+++ bird_sandr/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l!ojo2%&!p6$+u_x8j5ywx#n_+pq9eblzx3@p&885)q(g3cmuo'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['https://bird-sandr.herokuapp.com/', 'localhost']
 
